138ca31bc93bc37201e2d5443b68043e844b5030
Jukka Ikavalko <jpikavalko@gmail.com>
2020-05-05 11:21:46 +0300

    DatabaseTesting/DatabaseWrapper.cpp

--- Source/DatabaseTesting/DatabaseWrapper.cpp
+++ Source/DatabaseTesting/DatabaseWrapper.cpp
@@ -201,9 +201,3 @@ void UDatabaseWrapper::SetTableName(FString _tableName)
 {
 	TableName = _tableName;
 }
-
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DatabaseWrapper.h"
-
